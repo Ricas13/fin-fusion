8be9783f60b508532c4d626e7a69b82c1f796972
Ricas13 <56263921+Ricas13@users.noreply.github.com>
2026-08-24 18:52:38 +0100

    chore: remove obsolete app compatibility entrypoint

--- app.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-// Compatibility entry point for operators/scripts that historically invoked
-// `node app.js`.  All runtime composition now lives in src/application.js.
-const { start } = require('./src/application');
-start();
